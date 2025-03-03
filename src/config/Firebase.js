0966da687d9029def70398efc0205d7553bff946
radhibenafia <163842231+radhibenafia@users.noreply.github.com>
2025-03-03 14:51:34 +0100

    fourth

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -4,8 +4,15 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore from fire
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+    apiKey: "AIzaSyCvL5bBFDi9em_DYAnz93mcz5he1W3cKS4",
+    authDomain: "apiradhi-3a03f.firebaseapp.com",
+    projectId: "apiradhi-3a03f",
+    storageBucket: "apiradhi-3a03f.appspot.com",
+    messagingSenderId: "562975475551",
+    appId: "1:562975475551:web:b5307c27ed88713bd318ea",
+    measurementId: "G-V9NSCBBQJD"
 };
 
 // Initialize Firebase
