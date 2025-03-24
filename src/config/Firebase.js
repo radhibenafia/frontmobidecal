3070d911b5679db7f6184fe080c4e43bb61a3f6d
radhi ben afia <163842231+radhibenafia@users.noreply.github.com>
2025-03-24 09:56:50 +0100

    Update Firebase.js

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore from fire
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvL5bBFDi9em_DYAnz93mcz5he1W3cKS4",
-  authDomain: "apiradhi-3a03f.firebaseapp.com",
-  projectId: "apiradhi-3a03f",
-  storageBucket: "apiradhi-3a03f.appspot.com",
-  messagingSenderId: "562975475551",
-  appId: "1:562975475551:web:b5307c27ed88713bd318ea",
-  measurementId: "G-V9NSCBBQJD"
+  
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
