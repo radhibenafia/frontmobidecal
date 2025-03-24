39b7c62c0c35fb149b93d5b34334a8d9df5eb06e
radhibenafia <163842231+radhibenafia@users.noreply.github.com>
2025-03-24 09:54:41 +0100

    jj

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,12 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"; // Import Firestore from firebase/firestore
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  jj
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCvL5bBFDi9em_DYAnz93mcz5he1W3cKS4",
+  authDomain: "apiradhi-3a03f.firebaseapp.com",
+  projectId: "apiradhi-3a03f",
+  storageBucket: "apiradhi-3a03f.appspot.com",
+  messagingSenderId: "562975475551",
+  appId: "1:562975475551:web:b5307c27ed88713bd318ea",
+  measurementId: "G-V9NSCBBQJD"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app); // Correctly initialize Firestore
